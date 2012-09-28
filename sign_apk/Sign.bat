1fa232ed958942a2e7b444cfb6e58fbb29f3a09d
SpellCaster <spellcaster@bk.ru>
2012-09-28 17:38:23 +0400

    ! Sign.bat - убран интерактивный режим (добавлял багов и усложнял) ! Исправлена обработка ошибок, добавлены пояснения при ошибках (отправляют читать лог) ! Убраны "!" из текста на случай, если включено delayed expansion

--- sign_apk/Sign.bat
+++ sign_apk/Sign.bat
@@ -1,14 +1,14 @@
 :: Script for signing APK's
-:: Being called without parameters, the script is interactive.
-:: Being called with APK path as parameter, the script is not interactive.
-:: Sign and zipalign details are written to sign.log
+:: Parameters:
+::   %1 - FULL path to .apk file
+:: Sign and zipalign details and errors are written to sign.log
 :: Requirements:
 ::   You must have JDK installed, java.exe in PATH and modify the "set PATH" line
 ::     below to point to yours JDK\bin path
 ::   You also have to create your key store and set values in Key_cfg.bat to yours.
 ::   The folder with this script must not contain non-latin symbols
 
-@echo off
+:: @echo off
 
 :: Not changing calling process' PATH variable
 setlocal
@@ -25,19 +25,19 @@ call "%CDir%\Key_cfg.bat"
 :: Java
 java -version 2> nul
 if errorlevel 1 (
-	echo Java not installed!
+	echo Java not installed.
 	goto :Err
 )
 :: keytool
 keytool 2> nul
 if errorlevel 1 (
-	echo keytool.exe not found! Check your PATH settings inside the script, they must contain valid path to JDK\bin
+	echo keytool.exe not found. Check your PATH settings inside the script, they must contain valid path to JDK\bin
 	goto :Err
 )
 :: jarsigner
 jarsigner 2> nul 1> nul
 if errorlevel 1 (
-	echo jarsigner.exe not found! Check your PATH settings inside the script, they must contain valid path to JDK\bin
+	echo jarsigner.exe not found. Check your PATH settings inside the script, they must contain valid path to JDK\bin
 	goto :Err
 )
 :: zipalign
@@ -50,7 +50,7 @@ if not exist "%CDir%\zipalign.exe" (
 :: ! Magic ! We imitate Enter keys to accept default empty values to "Your name",
 :: "Your organisation" and other unneeded stuff which keytool is asking about.	
 if not exist "%KeyStore%" (
-	echo WARNING! %KeyStore% not found. Press any key to create it with the values from Key_cfg.bat or Ctrl-C to cancel.
+	echo WARNING. %KeyStore% not found. Press any key to create it with the values from Key_cfg.bat or Ctrl-C to cancel.
 	pause > nul
 	(echo.&echo.&echo.&echo.&echo.&echo.&echo yes) | keytool.exe -genkey -v -keystore "%KeyStore%" -alias %Alias% -keyalg RSA -keysize 2048 -validity 10000 -keypass %KeyPass% -storepass %StorePass%
     if errorlevel 1 (
@@ -59,20 +59,11 @@ if not exist "%KeyStore%" (
     )	
 )
 
-:: Need DelayedExpansion here, or apk_path is set to empty string. ARRRGH!
-
-if "%1%"=="" (
-    title APK signer
-:Prompt
-    setlocal EnableDelayedExpansion
-    set apk_path=
-    set /p apk_path=Input FULL path to the apk to sign or empty string to exit and hit Enter 
-    if "!apk_path!"=="" goto :Eof
-    :: remove extension from the path
-    set apk_path=!apk_path:~0,-4!
-    setlocal DisableDelayedExpansion
-) else (
-    set apk_path=%~dpn1%
+:: apk_path will contain full path to apk WITHOUT extension
+set apk_path=%~dpn1%
+if "!apk_path!"=="" (
+    echo apk path is empty
+    goto :Eof
 )
 
 :: Prepare
@@ -82,24 +73,25 @@ copy "%apk_path%.apk" "%apk_path%_tmp.apk" > nul
 :: Sign
 echo Signing...
 call jarsigner.exe -verbose -keystore "%KeyStore%" -sigalg MD5withRSA -digestalg SHA1 -storepass %StorePass% --keypass %KeyPass% "%apk_path%_tmp.apk" %Alias% > "%CDir%\sign.log"
+if errorlevel 1 (
+	echo ERROR. Something wrong happened. Check "%CDir%\sign.log" for details
+	goto :Err
+)
 
 :: zip align
-if %errorlevel% LEQ 0 (
-	echo Zip aligning...
-	call "%CDir%\zipalign.exe" -v 4 "%apk_path%_tmp.apk" "%apk_path%_signed.apk" >> "%CDir%\sign.log"
-	if %errorlevel% LEQ 0 (
-		echo Sign OK! Signed APK saved as "%apk_path%_signed.apk"
-	)
+echo Zip aligning...
+call "%CDir%\zipalign.exe" -v 4 "%apk_path%_tmp.apk" "%apk_path%_signed.apk" >> "%CDir%\sign.log"
+if errorlevel 1 (
+    echo ERROR. Something wrong happened. Check "%CDir%\sign.log" for details
+	goto :Err
+) else (
+	echo Sign OK. Signed APK saved as "%apk_path%_signed.apk"
 )
 
 :: Cleanup
 del "%apk_path%_tmp.apk" 2> nul
 
-if "%1%"=="" (
-    goto :Prompt
-) else (
-    goto :Eof
-)
+goto :Eof
 
 :Err
 pause
